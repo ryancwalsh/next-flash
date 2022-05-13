16dfa005bf6b0d06a24ea37a4d9b8dec4e72310c
Ryan Walsh <rcwalsh@gmail.com>
2022-05-13 15:17:17 -0400

    proof that setFlashVariable works on regular pages

--- pages/protected.tsx
+++ pages/protected.tsx
@@ -1,14 +1,13 @@
 import type { GetServerSideProps, NextPage } from 'next'
 import { getSession } from 'next-auth/react'
-import { getFlashSession } from '../lib/getFlashSession'
+import { setFlashVariable } from '../lib/getFlashSession'
 
 const ProtectedPage: NextPage = () => <h1>Protected Page</h1>
 
 const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
   const session = await getSession({ req })
   if (!session) {
-    const flashSession = await getFlashSession(req, res)
-    flashSession.flash = 'You must be logged in to access this page.'
+    await setFlashVariable(req, res, 'You must be logged in to access this page.')
     return { redirect: { destination: '/', permanent: false } }
   }
   return { props: {} }
